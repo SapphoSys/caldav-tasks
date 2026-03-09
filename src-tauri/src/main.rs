439ae4931405b72de2e325046608e250be7333dd
Chloe A <chloe@sapphic.moe>
2026-03-10 01:27:02 +0500

    chore: remove unused tauri-plugin-shell dependency

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -48,7 +48,6 @@ fn main() {
         .plugin(logging::build_logging_plugin().build())
         .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_process::init())
-        .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
